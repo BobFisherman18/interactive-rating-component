fcf1a6d6bcb48615272758932075e5fcb5cebcd2
BobFisherman18 <jonahrivera2010@hotmail.com>
2024-05-26 15:58:24 -0700

    added screenshots of the webpages; user input can dynamically change the paragraph in the Thank Yoou webpage based on the rating

--- rating-script.js
+++ rating-script.js
@@ -1,9 +1,3 @@
-const RATE1 = document.getElementById(`rate1`).textContent;
-const RATE2 = document.getElementById(`rate2`).textContent;
-const RATE3 = document.getElementById(`rate3`).textContent;
-const RATE4 = document.getElementById(`rate4`).textContent;
-const RATE5 = document.getElementById(`rate5`).textContent;
-
 const RATE1BUTTON = document.getElementById('rate1');
 const RATE2BUTTON = document.getElementById('rate2');
 const RATE3BUTTON = document.getElementById('rate3');
@@ -12,16 +6,16 @@ const RATE5BUTTON = document.getElementById('rate5');
 let buttonsClicked = document.querySelectorAll(`span`);
 
 
-console.log(buttonsClicked);
-
     for (let i = 0; i < buttonsClicked.length; i++) {
         buttonsClicked[i].addEventListener('click', event => {
             switch(buttonsClicked[i].textContent) {
+
                 case "1":
                 //Button 1 Clicked
                 RATE1BUTTON.style.backgroundColor = "hsl(25, 97%, 53%)";
                 RATE1BUTTON.style.color = " hsl(0, 0%, 100%)";
                 localStorage.setItem("rating", "1");
+                localStorage.setItem("rateDesc", "We're sorry about the poor experience. Please tell us what we can do to better ourselves.");
             
                 //All Buttons NOT clicked
                 RATE2BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
@@ -33,11 +27,13 @@ console.log(buttonsClicked);
                 RATE5BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
                 RATE5BUTTON.style.color = "hsl(217, 12%, 63%)";
                 break;
+
                 case "2":
                 //Button 2 Clicked
                 RATE2BUTTON.style.backgroundColor = "hsl(25, 97%, 53%)";
                 RATE2BUTTON.style.color = " hsl(0, 0%, 100%)";
                 localStorage.setItem("rating", "2");
+                localStorage.setItem("rateDesc", "We're sorry about the poor experience. Please tell us what we can do to better ourselves.");
 
                 //All Buttons NOT clicked
                 RATE1BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
@@ -49,11 +45,14 @@ console.log(buttonsClicked);
                 RATE5BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
                 RATE5BUTTON.style.color = "hsl(217, 12%, 63%)";
                 break;
+
                 case "3":
                 //Button 3 Clicked
                 RATE3BUTTON.style.backgroundColor = "hsl(25, 97%, 53%)";
                 RATE3BUTTON.style.color = " hsl(0, 0%, 100%)";
                 localStorage.setItem("rating", "3");
+                localStorage.setItem("rateDesc", "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!");
+                
 
                 //All Buttons NOT clicked
                 RATE1BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
@@ -65,11 +64,13 @@ console.log(buttonsClicked);
                 RATE5BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
                 RATE5BUTTON.style.color = "hsl(217, 12%, 63%)";
                 break;
+
                 case "4":
                 //Button 4 Clicked
                 RATE4BUTTON.style.backgroundColor = "hsl(25, 97%, 53%)";
                 RATE4BUTTON.style.color = " hsl(0, 0%, 100%)";
                 localStorage.setItem("rating", "4");
+                localStorage.setItem("rateDesc", "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!");
 
                 //All Buttons NOT clicked
                 RATE1BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
@@ -81,11 +82,13 @@ console.log(buttonsClicked);
                 RATE5BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
                 RATE5BUTTON.style.color = "hsl(217, 12%, 63%)";
                 break;
+
                 case "5":
                 //Button 5 Clicked
                 RATE5BUTTON.style.backgroundColor = "hsl(25, 97%, 53%)";
                 RATE5BUTTON.style.color = " hsl(0, 0%, 100%)";
                 localStorage.setItem("rating", "5");
+                localStorage.setItem("rateDesc", "Thank you for the perfect score!! We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!");
 
                 //All Buttons NOT clicked
                 RATE1BUTTON.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
@@ -100,79 +103,3 @@ console.log(buttonsClicked);
             }
         })
     }
-    $(document).ready(function(){
-        $("#rate2").click(function(){
-            console.log("rate 2 Clicked");
-        });
-      });
-
-      function myFunction() {
-        var w2 = window.open("./thank-you.html");
-        w2.document.getElementById('rateDesc').innerHTML = "Hi";
-        console.log(w2.document.title);
-        
-      }
-
-
-  
-
-/*
-switch(RATE2) {
-    case "1":
-        RATE1BUTTON.style.backgroundColor = "hsl(25, 97%, 53%)";
-        RATE1BUTTON.style.color = " hsl(0, 0%, 100%)";
-        break;
-    case "2":
-    RATE2BUTTON.style.backgroundColor = "hsl(25, 97%, 53%)";
-    RATE2BUTTON.style.color = " hsl(0, 0%, 100%)";
-    break;
-}
-
-
-/*
-RATE1.onclick = function rating1Function() {
-    RATE1.style.backgroundColor = "hsl(25, 97%, 53%)";
-    RATE1.style.color = " hsl(0, 0%, 100%)";
-    rateClicked = true;
-    if(rateClicked = true) {
-        RATE2.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
-        RATE2.style.color = "hsl(217, 12%, 63%)";
-    }
-    /*
-    else if (rateClicked = true) {
-        RATE3.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
-        RATE3.style.color = "hsl(217, 12%, 63%)";
-    }
-    */
-   /*
-}
-
-RATE2.onclick = function rating2Function() {
-    RATE2.style.backgroundColor = "hsl(25, 97%, 53%)";
-    RATE2.style.color = " hsl(0, 0%, 100%)";
-    if(rateClicked = true) {
-        RATE1.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
-        RATE1.style.color = "hsl(217, 12%, 63%)";
-    }
-    /*
-    else if (rateClicked = true) {
-        RATE3.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
-        RATE3.style.color = "hsl(217, 12%, 63%)";
-    }
-    */
-   /*
-}
-/*
-RATE3.onclick = function rating3Function() {
-    RATE3.style.backgroundColor = "hsl(25, 97%, 53%)";
-    RATE3.style.color = " hsl(0, 0%, 100%)";
-    if(rateClicked = true) {
-        RATE1.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
-        RATE1.style.color = "hsl(217, 12%, 63%)";
-    }
-    else if (rateClicked = true) {
-        RATE2.style.backgroundColor = "hsla(216, 12%, 54%, 0.1)";
-        RATE2.style.color = "hsl(217, 12%, 63%)";
-    }
-}
-*/
